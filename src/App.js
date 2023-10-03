18e007aeebe88ee36634388f420cf12fc9f4351f
NeeteshNG <neeteshng15@gmail.com>
2023-10-03 18:34:48 +0530

    Converted Functional Components into Class Components

--- src/App.js
+++ src/App.js
@@ -1,98 +1,125 @@
 import "./App.css";
-import { useState } from "react";
-import React from "react";
+import React, { Component } from "react";
 import Table from "./Components/Table";
 import Form from "./Components/Form";
 
-function App() {
-  const [fullData, setFullData] = useState([
-    { id: 1, name: "Neetesh", age: "22", city: "Ashokanagar", gender: "Male" },
-    { id: 2, name: "Akash", age: "25", city: "Bhopal", gender: "Male" },
-    { id: 3, name: "Rupa", age: "32", city: "Mumbai", gender: "Female" },
-  ]);
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-  
-  const [isPopupVisible, setPopupVisible] = useState(false);
-  const [isTableVisible, setTableVisible] = useState(false);
-  const [newData, setNewData] = useState({ id: null, name: "", age: "", city: "", gender: "" });
-  
-  const handleAddClick = () => {
-    setPopupVisible(true);
-    setNewData({ id: null, name: "", age: "", city: "", gender: "" });
-  };
-  
-  const handleEditClick = (person) => {
-    setPopupVisible(true);
-    setNewData({ ...person });
-  };
-
-  const handleShowClick = () => {
-    setTableVisible(true);
-  };
-  
-  const handleCloseClick = () => {
-    setTableVisible(false);
-  };
-
-  const handleDeleteClick = (id) => {
-    const updatedData = fullData.filter((person) => person.id !== id);
-    setFullData(updatedData);
-  };
-  
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setNewData(values => ({...values, [name]: value}))
+    this.state = {
+      fullData: [
+        { id: 1, name: "Neetesh", age: "22", city: "Ashokanagar", gender: "Male" },
+        { id: 2, name: "Akash", age: "25", city: "Bhopal", gender: "Male" },
+        { id: 3, name: "Rupa", age: "32", city: "Mumbai", gender: "Female" },
+      ],
+      isPopupVisible: false,
+      isTableVisible: false,
+      newData: { id: null, name: "", age: "", city: "", gender: "" },
+    };
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (newData.name && newData.age && newData.city && newData.gender !== ""){
-      if (newData.id === null) {
+  handleAddClick = () => {
+    this.setState({
+      isPopupVisible: true,
+      newData: { id: null, name: "", age: "", city: "", gender: "" },
+    });
+  };
 
+  handleEditClick = (person) => {
+    this.setState({
+      isPopupVisible: true,
+      newData: { ...person },
+    });
+  };
+
+  handleShowClick = () => {
+    this.setState({
+      isTableVisible: true,
+    });
+  };
+
+  handleCloseClick = () => {
+    this.setState({
+      isTableVisible: false,
+    });
+  };
+
+  handleDeleteClick = (id) => {
+    const updatedData = this.state.fullData.filter((person) => person.id !== id);
+    this.setState({
+      fullData: updatedData,
+    });
+  };
+
+  handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState((prevState) => ({
+      newData: { ...prevState.newData, [name]: value },
+    }));
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { newData, fullData } = this.state;
+    if (newData.name && newData.age && newData.city && newData.gender !== "") {
+      if (newData.id === null) {
         const newId = fullData.length + 1;
-        setFullData([...fullData, { ...newData, id: newId }]);
+        this.setState((prevState) => ({
+          fullData: [...prevState.fullData, { ...newData, id: newId }],
+          isPopupVisible: false,
+        }));
       } else {
-  
         const updatedData = fullData.map((person) =>
           person.id === newData.id ? newData : person
         );
-        setFullData(updatedData);
-        console.log(typeof(fullData.name))
+        this.setState({
+          fullData: updatedData,
+          isPopupVisible: false,
+        });
       }
-      setPopupVisible(false);
     }
   };
 
-  return (
-    <div className="App">
-      <button className="buttons" onClick={handleShowClick}>Show Table</button>
+  render() {
+    const { fullData, isTableVisible, isPopupVisible, newData } = this.state;
 
-      {isTableVisible && (
-      <>
-        <button className="buttons" onClick={handleCloseClick}>Close</button>
-        <Table
-          fullData={fullData} 
-          handleDeleteClick={handleDeleteClick} 
-          handleEditClick={handleEditClick}
-        />
-      </>
-      )}
+    return (
+      <div className="App">
+        <button className="buttons" onClick={this.handleShowClick}>
+          Show Table
+        </button>
 
-      <button className="buttons" onClick={handleAddClick}>Sign Up</button>
+        {isTableVisible && (
+          <>
+            <button className="buttons" onClick={this.handleCloseClick}>
+              Close
+            </button>
+            <Table
+              fullData={fullData}
+              handleDeleteClick={this.handleDeleteClick}
+              handleEditClick={this.handleEditClick}
+            />
+          </>
+        )}
 
-      {isPopupVisible && (
-        <>
-          <Form
-            newData={newData} 
-            handleChange={handleChange}
-            handleSubmit={handleSubmit}
-          />
-        </>
-      )}
-    </div>
-    
-  );
+        <button className="buttons" onClick={this.handleAddClick}>
+          Sign Up
+        </button>
+
+        {isPopupVisible && (
+          <>
+            <Form
+              newData={newData}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+            />
+          </>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;

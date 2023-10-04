a2adf8d5602614f24e1ae92efef5ac125e42980a
NeeteshNG <neeteshng15@gmail.com>
2023-10-04 18:30:08 +0530

    Made Inputs to be Required Field

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -14,7 +14,8 @@ class Form extends Component{
                 placeholder='Enter Your Name : '
                 name="name"
                 value={newData.name}
-                onChange={handleChange}/>
+                onChange={handleChange}
+                required/>
               </label>
             <label>Age :-- <br></br>
               <input 
@@ -23,7 +24,8 @@ class Form extends Component{
                 placeholder='Enter Your Age'
                 name="age"
                 value={newData.age}
-                onChange={handleChange}/>
+                onChange={handleChange}
+                required/>
             </label>
             <label>City :-- <br></br>
               <input 
@@ -32,11 +34,12 @@ class Form extends Component{
                 placeholder='Enter City'
                 name="city"
                 value={newData.city}
-                onChange={handleChange}/>
+                onChange={handleChange}
+                required/>
             </label>
             <label>Gender :-- <br></br>
-            <select name="gender" value={newData.gender} onChange={handleChange}>
-              <option name="Select" value={newData.select}>Select</option>
+            <select name="gender" value={newData.gender} onChange={handleChange} required>
+              <option name="Select">Select</option>
               <option name="Male" value={newData.male}>Male</option>
               <option name="Female" value={newData.female}>Female</option>
               <option name="Other" value={newData.other}>Other</option>
